03b8f65298d8d8d378ff4ef9aad809e2f0160cc2
g <gaolmos1@gmail.com>
2021-11-28 22:13:32 -0300

    Add older files

---
+++ website/auth.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Logout yeah </h1>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstNamee = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            # add user to db
+    return render_template("sign_up.html")
